4d12d7287fe001fc390cf038aecd17ba34f6b536
higorm22 <higormoraes22@gmail.com>
2022-08-10 12:06:30 -0300

    Ignit Courses - list e validacao 10/08/2022

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,12 +4,25 @@ import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
+
 categoriesRoutes.post ("/", (request, response) => {
   const { name, description } = request.body;
+
+  const categoryAleadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAleadyExists) {
+    return response.status(400).json({error: "Categoria inexistente!"})
+  }
 
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) =>{
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
